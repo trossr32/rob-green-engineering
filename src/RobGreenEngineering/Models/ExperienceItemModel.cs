9c4eed6ab9fc932bab89ab6ed44ecb93ed71eeec
Rob Green <robert.green@yourkeys.com>
2023-04-09 21:03:22 +0100

    Fix error on link to new tab

---
+++ src/RobGreenEngineering/Models/ExperienceItemModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RobGreenEngineering.Models;
+
+public class ExperienceItemModel
+{
+    public string Image { get; set; }
+    public string DateRange { get; set; }
+    public string Role { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+}
