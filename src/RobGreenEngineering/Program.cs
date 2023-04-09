a10a074303a07742b70bafcb8698968a61d269fd
Rob Green <robert.green@yourkeys.com>
2023-04-09 19:51:54 +0100

    Remove auth testing

--- src/RobGreenEngineering/Program.cs
+++ src/RobGreenEngineering/Program.cs
@@ -1,7 +1,5 @@
 using Blazored.LocalStorage;
 using RobGreenEngineering;
-using RobGreenEngineering.Providers;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,10 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddAuthorizationCore();
-
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 builder.Services.AddBlazoredLocalStorage();
 
